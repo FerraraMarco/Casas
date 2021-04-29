6a3bf336091d4352535d04786930957b7a5c6bb9
FerraraMarco <37107101+FerraraMarco@users.noreply.github.com>
2021-04-29 12:41:47 +0200

    Update arima script

--- arima.py
+++ arima.py
@@ -1,10 +1,11 @@
-from pylab import rcParams
 import warnings
 import itertools
+import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import statsmodels.api as sm
 import matplotlib
+from pylab import rcParams
 
 warnings.filterwarnings("ignore")
 plt.style.use('fivethirtyeight')
@@ -13,42 +14,79 @@ matplotlib.rcParams['xtick.labelsize'] = 12
 matplotlib.rcParams['ytick.labelsize'] = 12
 matplotlib.rcParams['text.color'] = 'k'
 
-dfTime = pd.read_csv('HH113-WeightedAVG.csv')
-dfTime['Date'] = pd.to_datetime(dfTime['Date'], format='%Y-%m-%d %H:%M:%S.%f')
-dfTimeResample = dfTime.resample('30T', on='Date').mean()
-dfTimeResample = dfTimeResample.fillna(0)
-dfTimeResample.to_csv('HH113-LightResampled.csv')
-dfTimeResample.plot(figsize=(15, 6))
+df = pd.read_csv("HH113-WeightedAVG.csv")
+df['Date'] = pd.to_datetime(df['Date'], format='%Y-%m-%d %H:%M:%S')
+y = df.copy()
+y.columns = ['Date', 'Lights']
+y = y.sort_values('Date')
+y = y.groupby('Date')['Lights'].sum().reset_index()
+y = y.set_index('Date')
+y.plot(figsize=(15, 6))
+
 rcParams['figure.figsize'] = 18, 8
-decomposition = sm.tsa.seasonal_decompose(dfTimeResample, period=30, model='additive')
+decomposition = sm.tsa.seasonal_decompose(y, model='additive', period=30)
 fig = decomposition.plot()
 plt.show()
-
-dfSeasonal = pd.DataFrame(columns=["Order", "Seasonal order", "Result"])
-
-p = d = q = range(0, 2)
+"""p = d = q = range(0, 2)
 pdq = list(itertools.product(p, d, q))
 seasonal_pdq = [(x[0], x[1], x[2], 12) for x in list(itertools.product(p, d, q))]
-i = 0
 for param in pdq:
     for param_seasonal in seasonal_pdq:
         try:
-            mod = sm.tsa.statespace.SARIMAX(dfTimeResample, order=param, seasonal_order=param_seasonal,
-                                            enforce_stationarity=False, enforce_invertibility=False)
+            mod = sm.tsa.statespace.SARIMAX(y,
+                                            order=param,
+                                            seasonal_order=param_seasonal,
+                                            enforce_stationarity=False,
+                                            enforce_invertibility=False)
             results = mod.fit()
-            dfSeasonal.loc[i, 'Order'] = param
-            dfSeasonal.loc[i, 'Seasonal order'] = param_seasonal
-            dfSeasonal.loc[i, 'Result'] = results.aic
-            i += 1
+            print('ARIMA{}x{}12 - AIC:{}'.format(param, param_seasonal, results.aic))
         except:
-            continue
-dfSeasonal.to_csv("ARIMA.csv", index=False)
-df = pd.read_csv("ARIMA.csv")
-row = df.loc[df['Result'] == df['Result'].min()]
-mod = sm.tsa.statespace.SARIMAX(dfTimeResample, order=(1, 0, 1), seasonal_order=(1, 1, 1, 12),
-                                enforce_stationarity=False, enforce_invertibility=False)
+            continue"""
+mod = sm.tsa.statespace.SARIMAX(y,
+                                order=(1, 1, 1),
+                                seasonal_order=(1, 1, 0, 12),
+                                enforce_stationarity=False,
+                                enforce_invertibility=False)
 results = mod.fit()
 print(results.summary().tables[1])
-
 results.plot_diagnostics(figsize=(16, 8))
+plt.show()
+pred = results.get_prediction(start=pd.to_datetime('2012-09-15 07:30:00'), dynamic=False)
+pred_ci = pred.conf_int()
+predicted = pred.predicted_mean
+predicted = predicted.reset_index()
+predicted.columns = ['Date', 'LightsForecasted']
+predicted.loc[predicted['LightsForecasted'] < 0, 'LightsForecasted'] = 0
+predicted = predicted.set_index('Date')
+ax = y['2011':].plot(label='observed')
+predicted.plot(ax=ax, label='One-step ahead Forecast', alpha=.7, figsize=(14, 7))
+ax.fill_between(pred_ci.index,
+                pred_ci.iloc[:, 0],
+                pred_ci.iloc[:, 1], color='k', alpha=.2)
+ax.set_xlabel('Date')
+ax.set_ylabel('Light')
+plt.legend()
+plt.show()
+
+y_forecasted = predicted
+y_truth = y['2012-09-15 07:30:00':]
+y_forecasted = y_forecasted.reset_index()
+y_forecasted.columns = ['Date', 'Lights']
+y_truth = y_truth.reset_index()
+mse = i = 0
+for i in range(len(y_forecasted)):
+    mse += ((y_forecasted.loc[i, 'Lights'] - y_truth.loc[i, 'Lights']) ** 2)
+mse = mse / i
+print('The Mean Squared Error of our forecasts is {}'.format(round(mse, 2)))
+print('The Root Mean Squared Error of our forecasts is {}'.format(round(np.sqrt(mse), 2)))
+pred_uc = results.get_forecast(steps=48)
+pred_ci = pred_uc.conf_int()
+ax = y.plot(label='observed', figsize=(14, 7))
+pred_uc.predicted_mean.plot(ax=ax, label='Forecast')
+ax.fill_between(pred_ci.index,
+                pred_ci.iloc[:, 0],
+                pred_ci.iloc[:, 1], color='k', alpha=.25)
+ax.set_xlabel('Date')
+ax.set_ylabel('Lights')
+plt.legend()
 plt.show()
